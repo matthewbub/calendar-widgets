f71e6d61a987921afa144174b0287a91b25a3741
matéo! <53072963+yeahmat@users.noreply.github.com>
2022-06-02 14:02:39 +0000

    init tailwindcss daisyui

--- lib/developer/pages/index.jsx
+++ lib/developer/pages/index.jsx
@@ -1,6 +1,27 @@
 const Home = () => {
   return (
-    <h1>Hi</h1>
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-m lg:max-w-2xl xl:max-w-3xl">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">@whilethiscompiles/calendar</h1>
+            <p className="py-6">
+            A simple API intended to return a list of calendar dates for a given year. Built with [day.js](https://day.js.org/) & typescript.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mockup-code">
+        <pre data-prefix="~">
+          <code>
+            Magnam dolore beatae necessitatibus nemopsum itaque sit. Et porro quae qui
+            et et dolore ratione.
+          </code>
+        </pre>
+      </div>
+    </div>
   )
 }
 
