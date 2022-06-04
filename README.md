64dc7586d3ee1eb043ce82ff13f41c047f41b1d6
matéo! <53072963+yeahmat@users.noreply.github.com>
2022-06-04 01:01:31 +0000

    went with mantine - UI MVP established

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 A simple API intended to return a list of calendar dates for a given year. Built with [day.js](https://day.js.org/) & typescript.
 
-## Nodejs
+## Usage
+
+### Via Nodejs
 
 ```shell
 npm install @whilethiscompiles/calendar
@@ -15,7 +17,7 @@ const {calendar} = require('@whilethiscompiles/calendar')
 calendar(1999); // {january:{count:31,collection:{'1':'1999-1-01','2':'1999-1-02','3':'1999-1-03','4':'1999-1-04','5':'1999-1-05','6':'1999-1-06','7':'1999-1-07','8':'1999-1-08','9':'1999-1-09','10':'1999-1-10','11':'1999-1-11','12':'1999-1-12','13':'1999-1-13','14':'1999-1-14','15':'1999-1-15','16':'1999-1-16','17':'1999-1-17','18':'1999-1-18','19':'1999-1-19','20':'1999-1-20','21':'1999-1-21','22':'1999-1-22','23':'1999-1-23','24':'1999-1-24','25':'1999-1-25','26':'1999-1-26','27':'1999-1-27','28':'1999-1-28','29':'1999-1-29','30':'1999-1-30','31':'1999-1-31'}},february:{count:28,collection:{'1':'1999-2-01','2':'1999-2-02',....
 ```
 
-## HTTP
+### Via HTTP
 
 A public endpoint is available via the GET method & accepts 1 query paramter `year=YYYY` returns a JSON response
 
@@ -25,4 +27,9 @@ https://calendar.whilethiscompiles.com/api/calendar?year=YYYY
 
 ## Contributing 
 
-This project was build entirely with GitHub's Codespaces & VSCode 
+This project was build entirely with GitHub's Codespaces & VSCode. 
+
+### This repo
+- `/lib/calendar` - NPM Package
+- `/lib/calendar-tests` - Test suites for calendar as a package
+- `/lib/developer` - Landing page, public API, documentation. Built with Next.js
--- lib/developer/pages/index.jsx
+++ lib/developer/pages/index.jsx
@@ -1,26 +1,121 @@
+import { useEffect, useState } from 'react';
+import { 
+  Stack, 
+  Container, 
+  Title, 
+  JsonInput,
+  NumberInput, 
+  Text,
+  Code,
+  SegmentedControl,
+  Anchor,
+  Center,
+  Button,
+  Group
+} from '@mantine/core';
+import {calendar} from '@whilethiscompiles/calendar'
+import {BrandGithub,BrandOpenSource} from 'tabler-icons-react';
 const Home = () => {
+  const [installValue, setInstallValue] = useState('npm');
+  const [tryItOutValue, setTryItOutValue] = useState('sdk');
+  const [demoSdkYear, setDemoSdkYear] = useState(1999);
+  const [output, setOutput] = useState(calendar(1999));
+
+  useEffect(() => {
+    setOutput(calendar(demoSdkYear))
+  }, [demoSdkYear]);
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-m lg:max-w-2xl xl:max-w-3xl">
-            <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">@whilethiscompiles/calendar</h1>
-            <p className="py-6">
-            A simple API intended to return a list of calendar dates for a given year. Built with [day.js](https://day.js.org/) & typescript.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <Container size="xs" px="xs" my='xl' style={{height: '500px'}}>
+        <Center style={{height: '100%'}}>
+          <Stack align="center" justify="center">
+            <Title order={1}>@whilethiscompiles/calendar</Title>
+            <Text size="lg" align="center">
+            A simple API intended to return a list of calendar dates for a given year. Built with <Anchor href="https://day.js.org/" target="_blank">day.js</Anchor> & typescript.
+            </Text>
+            <Group>
+              <Button 
+                component="a"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/yeahmat/calendar"
+                leftIcon={<BrandGithub size={18} />}
+                color="cyan"
+              >
+                GitHub
+              </Button>
+              <Button               
+                component="a"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/yeahmat/calendar/issues"
+                leftIcon={<BrandOpenSource size={18} />}
+                color="cyan"
+                variant='outline'
+              >
+                Open Source
+              </Button>
+            </Group>
+          </Stack>
+        </Center>
+      </Container>
 
-      <div className="mockup-code">
-        <pre data-prefix="~">
-          <code>
-            Magnam dolore beatae necessitatibus nemopsum itaque sit. Et porro quae qui
-            et et dolore ratione.
-          </code>
-        </pre>
-      </div>
+      <Container size="xs" px="xs" mt='xl'>
+        <Stack>
+          <Title order={3}>Install</Title>
+          <div>
+            <SegmentedControl 
+              value={installValue}
+              onChange={setInstallValue}
+              data={[
+                {label: 'npm', value: 'npm'},
+                {label: 'yarn', value: 'yarn'}
+              ]}
+            />
+          </div>
+          <div>
+            {installValue === "npm" ? (
+              <Code>npm install @whilethiscompiles/calendar</Code>
+            ) : (
+              <Code>yarn add @whilethiscompiles/calendar</Code>
+            )}
+          </div>
+        </Stack>
+      </Container>
+      <Container size="xs" px="xs" mt='xl'>
+        <Stack>
+          <Title order={3}>Try it out</Title>
+          <div>
+            <NumberInput
+              label="Enter Year"
+              description="Get a list of dates for a given year."
+              min={1970}
+              max={3000}
+              value={demoSdkYear}
+              onChange={setDemoSdkYear}
+            />
+          </div>
+          <div>
+            <SegmentedControl 
+              value={tryItOutValue}
+              onChange={setTryItOutValue}
+              data={[
+                {label: 'SDK', value: 'sdk'},
+                {label: 'REST API', value: 'restapi'}
+              ]}
+            />
+          </div>
+          <Code block>{`import {calendar} from '@whilethiscompiles/calendar';
+
+calendar(${demoSdkYear})`}
+          </Code>
+          <JsonInput
+            minRows={14}
+            onChange={setOutput}
+            value={JSON.stringify(output, null, 2)}
+          />
+        </Stack>
+      </Container>
     </div>
   )
 }
