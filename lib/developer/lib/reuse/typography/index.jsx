44e9fb9889cec82ba86e737a05fb6c8b0386effa
9mbs <6matbub@gmail.com>
2023-03-17 06:40:53 -0700

    #43 - rm api

--- lib/developer/lib/reuse/typography/index.jsx
+++
@@ -1,9 +0,0 @@
-import { TypographyStylesProvider } from '@mantine/core';
-
-export default function Typography({innerHTML}) {
-  return (
-    <TypographyStylesProvider>
-      <div dangerouslySetInnerHTML={{ __html: innerHTML }} />
-    </TypographyStylesProvider>
-  );
-}
