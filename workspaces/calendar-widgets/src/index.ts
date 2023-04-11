7176f8a773a7521d696203ffef3bf430adbdcecb
9mbs <6matbub@gmail.com>
2023-04-11 14:20:33 -0700

    #70 create and test getDefaultLocale method

--- workspaces/calendar-widgets/src/index.ts
+++ workspaces/calendar-widgets/src/index.ts
@@ -1,11 +1,12 @@
-import {getDaysInMonth} from './utils/getDaysInMonth';
-import {isValidYear} from './utils/isValidYear';
-import {listDaysInMonth} from './utils/listDaysInMonth';
-import {locale} from './locale/index';
-import {getCalendarYear} from './utils/getCalendarYear';
-import {formatDate} from './utils/formatDate';
-import {isValidDay} from './utils/isValidDay';
-import {isValidMonth} from './utils/isValidMonth';
+import { getDaysInMonth } from './utils/getDaysInMonth';
+import { isValidYear } from './utils/isValidYear';
+import { listDaysInMonth } from './utils/listDaysInMonth';
+import { locale } from './locale/index';
+import { getCalendarYear } from './utils/getCalendarYear';
+import { formatDate } from './utils/formatDate';
+import { isValidDay } from './utils/isValidDay';
+import { isValidMonth } from './utils/isValidMonth';
+import { getDefaultLocale } from './utils/getDefaultLocale';
 
 export {
   getDaysInMonth,
@@ -15,5 +16,6 @@ export {
   formatDate,
   isValidYear,
   isValidDay,
-  isValidMonth
+  isValidMonth,
+  getDefaultLocale
 };
