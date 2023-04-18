875d99927fe38dbf83d31393beb2a65b5f285a76
9mbs <6matbub@gmail.com>
2023-04-18 09:45:26 -0700

    #115 JSDoc Draggable utils

--- workspaces/calendar-widgets/src/components/Draggable/index.tsx
+++ workspaces/calendar-widgets/src/components/Draggable/index.tsx
@@ -10,7 +10,12 @@ const Draggable: FC<{ dynamicRows: number }> = ({ dynamicRows }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const resizeRef = useRef<HTMLDivElement>(null);
 
-
+  /**
+   * Handles the mouse down event on the draggable component.
+   * 
+   * @param {MouseEvent<HTMLDivElement>} e - The mouse down event.
+   * @returns {void}
+   */
   const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
 
@@ -21,11 +26,24 @@ const Draggable: FC<{ dynamicRows: number }> = ({ dynamicRows }) => {
     }
   };
 
+  /**
+   * Handles the mouse up event on the draggable component.
+   *
+   * @returns {void}
+   */
   const handleMouseUp = () => {
     setDragging(false);
     setDraggingBottom(false);
   };
 
+  /**
+   * Handles the dragging of the draggable component.
+   *
+   * @param {MouseEvent<HTMLDivElement>} e - The mouse move event.
+   * @param {DOMRect} containerRect - The bounding client rect of the draggable container.
+   * @param {number} rowHeight - The height of a single row in the container.
+   * @returns {void}
+   */
   const handleDragging = (e: MouseEvent<HTMLDivElement>, containerRect: DOMRect, rowHeight: number) => {
     const quarterRow = rowHeight / 4;
     const posY = Math.max(0, Math.round((e.clientY - containerRect.top) / quarterRow) * quarterRow);
@@ -38,7 +56,14 @@ const Draggable: FC<{ dynamicRows: number }> = ({ dynamicRows }) => {
     }
   };
 
-
+  /**
+   * Handles the dragging of the draggable component from the bottom edge.
+   *
+   * @param {MouseEvent<HTMLDivElement>} e - The mouse move event.
+   * @param {DOMRect} containerRect - The bounding client rect of the draggable container.
+   * @param {number} rowHeight - The height of a single row in the container.
+   * @returns {void}
+   */
   const handleDraggingBottom = (e: MouseEvent<HTMLDivElement>, containerRect: DOMRect, rowHeight: number) => {
     const posY = e.clientY - containerRect.top;
     const row = Math.floor((posY + rowHeight / 2) / rowHeight);
@@ -53,7 +78,12 @@ const Draggable: FC<{ dynamicRows: number }> = ({ dynamicRows }) => {
     }
   };
 
-
+  /**
+   * Handles the mouse move event on the draggable component.
+   *
+   * @param {MouseEvent<HTMLDivElement>} e - The mouse move event.
+   * @returns {void}
+   */
   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
     const containerRect = containerRef.current!.getBoundingClientRect();
     const rowHeight = containerRect.height / dynamicRows;
