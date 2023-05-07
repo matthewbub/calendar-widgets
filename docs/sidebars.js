09ba0fac9dd4af4c6cf27b921b2a877258d1e762
9mbs <6matbub@gmail.com>
2023-05-07 04:55:26 -0700

    #231 rm roadmap, documenting `Calendar`

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,24 +1,10 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
-
-// @ts-check
-
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   docs: [
     'introduction',
     {
-      type: 'category',
-      label: 'Components',
-      items: ['Components/Calendar'],
+      type: 'doc',
+      label: 'Calendar',
+      id: 'calendar',
     },
     {
       type: 'category',
@@ -30,7 +16,6 @@ const sidebars = {
       label: 'Utilities',
       items: ['Utilities/formatDate', 'Utilities/getDaysInMonth', 'Utilities/isValidDay', 'Utilities/isValidMonth', 'Utilities/isValidYear'],
     },
-    'road-map',
     'code-of-conduct',
   ],
 };
--- src/examples/BasicCalendarV2.tsx
+++ src/examples/BasicCalendarV2.tsx
@@ -2,18 +2,44 @@ import React from 'react';
 import { Calendar } from '../react';
 import { CustomHeaderFooterRendererProps } from '../react/Calendar/Calendar.types';
 
+const ButtonStyles = {
+  backgroundColor: 'transparent',
+  border: '1px solid #ccc',
+  borderRadius: '4px',
+  padding: '4px 8px',
+  cursor: 'pointer',
+  outline: 'none',
+  fontSize: '.75em',
+} as React.CSSProperties;
+
+const CustomHeaderStyles = {
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  boxSizing: 'border-box'
+} as React.CSSProperties;
+
 const BasicCalendarV2 = () => {
   return (
     <Calendar
       date={new Date()}
+      showAdjacentDays
+      dayNameToolTips={['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
       customHeader={(props: CustomHeaderFooterRendererProps) => (
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', boxSizing: 'border-box' }}>
-          <button onClick={props.handlePrevMonth}>
-            Prev month ({props.prevMonth})
+        <div style={CustomHeaderStyles}>
+          <button
+            onClick={props.handlePrevMonth}
+            style={ButtonStyles}
+          >
+            <div dangerouslySetInnerHTML={{ __html: '&lsaquo;' }}></div>
           </button>
-          {/* <div>{props.currentDate.toLocaleDateString('en-US')}</div> */}
-          <button onClick={props.handleNextMonth}>
-            Next month ({props.nextMonth})
+          <div>{props.selectedMonth}/{props.selectedYear}</div>
+          <button
+            onClick={props.handleNextMonth}
+            style={ButtonStyles}
+          >
+            <div dangerouslySetInnerHTML={{ __html: '&rsaquo;' }}></div>
           </button>
         </div>
       )}
