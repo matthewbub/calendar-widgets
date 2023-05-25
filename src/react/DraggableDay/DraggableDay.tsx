b13342b97c8a6fe9d49e31ef3f18b18b0035bdb6
matéo! <6matbub@gmail.com>
2023-05-25 04:41:54 -0700

    Midas touches

--- src/react/DraggableDay/DraggableDay.tsx
+++ src/react/DraggableDay/DraggableDay.tsx
@@ -1,10 +1,8 @@
 import React, { FC, useState, useEffect, useRef, MouseEvent, useCallback } from 'react';
 import './Draggables.styles.css';
-import { ONE, ONE_HUNDRED, ZERO } from '../../constants';
 import { listHoursBetween, calculateHoursBetween } from '../../helpers';
 import { SingleDraggableProps, DraggableContentProps, RowProps, DraggablesProps } from './DraggableDay.types';
 import { CalendarWidgetsProvider, useCalendarWidgetsContext } from '../CalendarWidgetsContext/CalendarWidgetsContext';
-import { magicNumber } from '../../helpers/magicNumber';
 
 const useDraggable = (resizeRef: React.RefObject<HTMLDivElement>) => {
   const [dragging, setDragging] = useState(false);
@@ -50,22 +48,22 @@ const useMouseMove = (
 
       if (containerRect && rowHeight) {
         if (dragging) {
-          const quarterRow = rowHeight / magicNumber('4');
+          const quarterRow = rowHeight / 4;
 
-          const posY = Math.max(magicNumber('0'), Math.round((e.clientY - containerRect.top) / quarterRow) * quarterRow);
+          const posY = Math.max(0, Math.round((e.clientY - containerRect.top) / quarterRow) * quarterRow);
 
           const maxY = containerRect.height - draggableHeight * rowHeight;
           if (posY <= maxY) {
-            setCurrentPos({ x: magicNumber('0'), y: posY });
+            setCurrentPos({ x: 0, y: posY });
           } else {
-            setCurrentPos({ x: magicNumber('0'), y: maxY });
+            setCurrentPos({ x: 0, y: maxY });
           }
         } else if (draggingBottom) {
           const posY = e.clientY - containerRect.top;
           const remainingHeight = containerRect.height - currentPos.y;
 
           // Calculate the nearest 1/4 value
-          const quarterRow = rowHeight / magicNumber('4');
+          const quarterRow = rowHeight / 4;
           const newRowHeight = Math.round((posY - currentPos.y) / quarterRow) * quarterRow;
 
           if (newRowHeight >= quarterRow && newRowHeight <= remainingHeight) {
@@ -100,7 +98,7 @@ const DraggableContent: FC<DraggableContentProps> = ({
   >
     Drag Me
     <button onClick={(e) => {
-      e.stopPropagation();onDelete();
+      e.stopPropagation(); onDelete();
     }} className="delete-draggable-btn">Delete</button>
     <div ref={resizeRef} className="resize-handle" onMouseDown={handleMouseDown} />
   </div>
@@ -172,11 +170,11 @@ const Row: FC<RowProps> = ({ index, dynamicRows, time }) =>
     className="row"
     style={{
       height: `calc(100% / ${dynamicRows})`,
-      top: `${index * ONE_HUNDRED / dynamicRows}%`,
-      borderBottom: index !== dynamicRows - ONE ? '1px solid #000' : 'none'
+      top: `${index * 100 / dynamicRows}%`,
+      borderBottom: index !== dynamicRows - 1 ? '1px solid #000' : 'none'
     }}
   >
-    {time.length > magicNumber('0') && <span>{time}</span>}
+    {time.length > 0 && <span>{time}</span>}
   </div>
   ;
 
@@ -184,12 +182,12 @@ const DraggableDayComponent: FC<DraggablesProps> = ({ startRow, endRow, initialD
   const dynamicRows = calculateHoursBetween(startRow, endRow);
   const times = listHoursBetween(startRow, endRow);
 
-  const [currentPos, setCurrentPos] = useState<{ x: number; y: number }>({ x: ZERO, y: ZERO });
-  const [draggableHeight, setDraggableHeight] = useState<number>(ONE);
+  const [currentPos, setCurrentPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [draggableHeight, setDraggableHeight] = useState<number>(1);
   const containerRef = useRef<HTMLDivElement>(null);
   const resizeRef = useRef<HTMLDivElement>(null);
   const { dragging, draggingBottom, handleMouseUp } = useDraggable(resizeRef);
-  const [defaultHeight, setDefaultHeight] = useState<number>(magicNumber('1'));
+  const [defaultHeight, setDefaultHeight] = useState<number>(1);
 
   const handleMouseMove = useMouseMove(
     dragging,
@@ -204,22 +202,22 @@ const DraggableDayComponent: FC<DraggablesProps> = ({ startRow, endRow, initialD
   const { draggableDays, setDraggableDays } = useCalendarWidgetsContext();
 
   useEffect(() => {
-    setDraggableDays(initialDraggables || [{ id: magicNumber('1'), position: { x: ZERO, y: ZERO }, height: ONE }]);
+    setDraggableDays(initialDraggables || [{ id: 1, position: { x: 0, y: 0 }, height: 1 }]);
   }, [initialDraggables]);
 
   const defaultDraggables = (() => {
     if (initialDraggables) {
-      return Math.max(...initialDraggables.map((d) => d.id)) + magicNumber('1');
+      return Math.max(...initialDraggables.map((d) => d.id)) + 1;
     }
-    return magicNumber('1');
+    return 1;
   })();
 
   const [nextId, setNextId] = useState<number>(defaultDraggables);
 
   const addDraggable = () => {
     setDraggableDays([...draggableDays,
-      { id: nextId, position: { x: ZERO, y: ZERO }, height: defaultHeight }]);
-    setNextId(nextId + magicNumber('1'));
+    { id: nextId, position: { x: 0, y: 0 }, height: defaultHeight }]);
+    setNextId(nextId + 1);
   };
 
   const deleteDraggable = (id: number) => {
@@ -251,7 +249,7 @@ const DraggableDayComponent: FC<DraggablesProps> = ({ startRow, endRow, initialD
             min="1"
             max={dynamicRows}
             value={defaultHeight}
-            onChange={(e) => setDefaultHeight(Math.max(magicNumber('1'), Math.min(parseInt(e.target.value), dynamicRows)))}
+            onChange={(e) => setDefaultHeight(Math.max(1, Math.min(parseInt(e.target.value), dynamicRows)))}
           />
         </label>
       </form>
@@ -261,7 +259,7 @@ const DraggableDayComponent: FC<DraggablesProps> = ({ startRow, endRow, initialD
       </button>
 
       <div className="draggable-container" ref={containerRef}>
-        {rows.length > ZERO && rows.map((_, i) =>
+        {rows.length > 0 && rows.map((_, i) =>
           <Row
             key={i}
             index={i}
