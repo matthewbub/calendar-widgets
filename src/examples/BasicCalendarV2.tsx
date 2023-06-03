9b6fe44509be4b2e717a686152981945e324f751
matéo! <6matbub@gmail.com>
2023-06-03 07:01:14 -0700

    demo: localized day names and day name tool tips

--- src/examples/BasicCalendarV2.tsx
+++ src/examples/BasicCalendarV2.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Calendar } from '../react';
-import { CustomHeaderFooterRendererProps } from '../react/Calendar/Calendar.types';
+import {CustomDate, CustomHeaderFooterRendererProps} from '../react/Calendar/Calendar.types';
 import { newDate } from '../helpers/newDate';
 
 /** Styles */
 // import '../../styles/Calendar-grid.css';
-import { useWindowSize } from './helpers';
 
 const ButtonStyles = {
   backgroundColor: 'transparent',
@@ -51,37 +50,66 @@ const CustomHeader = (props: CustomHeaderFooterRendererProps) => (
   </div>
 );
 
-const CustomDay = ({ date, isCurrentDay, baseStyles }: {
+const CustomDay = ({ date, isCurrentDay, baseStyles, customDate, inSelectedMonth }: {
   date: Date,
   isCurrentDay: boolean,
   baseStyles: React.CSSProperties;
+  customDate?: CustomDate;
+  inSelectedMonth: boolean;
 }) => (
-  <div style={{ ...baseStyles, height: '34px', textAlign: 'center' }}>
-    <p style={{ fontSize: '24px' }}>{date.getDate()}</p>
-    {isCurrentDay && <span style={{ color: 'red' }}>*</span>}
+  <div style={{ ...baseStyles, height: 'fit-content', textAlign: 'center' }}>
+    <div style={{
+      border: '1px solid #ccc',
+      position: 'relative',
+      '&:hover': {
+        'background': '#000'
+      }
+    }}>
+      <p style={{ fontSize: '24px', opacity: inSelectedMonth ? '100%' : '50%' }}>{date.getDate()}</p>
+      {isCurrentDay && <span style={{ color: 'red', position: 'absolute', top: '4px', right: '4px' }}>*</span>}
+      {customDate && <span style={{ color: 'red', position: 'absolute', bottom: '4px', right: '4px' }}>Event</span>}
+    </div>
   </div>
 );
 
-const CustomDayName = ({ label, baseStyles }: {
+const CustomDayName = ({ label, baseStyles, tooltip }: {
   label: string,
-  baseStyles: React.CSSProperties
+  baseStyles: React.CSSProperties,
+  tooltip?: string
 }) => (
-  <div style={{ ...baseStyles, height: '34px', textAlign: 'center' }}>
-    <p style={{ fontSize: '24px' }}>{label}</p>
+  <div
+    style={{ ...baseStyles, height: 'fit-content', textAlign: 'center' }}
+    title={tooltip ? tooltip : label}
+  >
+    <div style={{
+      border: '1px solid #ccc'
+    }}>
+      <p style={{ fontSize: '24px' }}>{label}</p>
+    </div>
+
   </div>
 );
 
 const BasicCalendarV2 = () => {
-  const { height } = useWindowSize();
+  function dayNameFactory(weekdayFormat: 'short' | 'long' | 'narrow' | undefined) {
+    const daysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    const localizedDaysOfWeek = daysOfWeek.map(day => {
+      const date = new Date(2023, 0, daysOfWeek.indexOf(day) + 1);
+      return date.toLocaleDateString('en-US', { weekday: weekdayFormat || 'short' });
+    });
+    return localizedDaysOfWeek;
+  }
+
   return (
     <Calendar
       date={new Date()}
       showAdjacentDays
-      dayNameToolTips={['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
+      dayNames={dayNameFactory('short')}
+      dayNameToolTips={dayNameFactory('long')}
       customHeader={CustomHeader}
       customDay={CustomDay}
       customDayName={CustomDayName}
-      layout={'flex'}
+      layout='flex'
       customDates={[{
         name: 'Lisa\'s Birthday',
         date: newDate(2023, 5, 10),
@@ -90,7 +118,6 @@ const BasicCalendarV2 = () => {
       }]}
       style={{
         width: '95%',
-        height: height - 100 + 'px',
         display: 'flex',
         flexWrap: 'wrap'
       }}
