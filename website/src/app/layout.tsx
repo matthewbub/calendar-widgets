d047ad27f60054e779ad10ccdce349ae429d7b3c
matéo! <6matbub@gmail.com>
2023-06-02 18:31:07 -0700

    set `@calendar-widgets/eslint-config` in website

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Calendar Widgets',
@@ -17,5 +17,5 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
